61d029a04b7708e046c78f76f6de1e9a5488744d
Takashi Yoshi <takashi@yoshi.email>
2017-05-06 18:33:31 +0200

    Implement hash_file function

--- src/filehelper.cxx
+++ src/filehelper.cxx
@@ -34,3 +34,75 @@ bool FileHelper::isValidRepoPath(const std::string& path) {
 
 	return S_ISDIR(st.st_mode);
 }
+
+std::string FileHelper::hash_file(const std::string& filepath) {
+#if HAVE_CRYPTO
+#if CRYPTO_WITH_SHA256
+	return ("sha256:" + FileHelper::libcrypto_hash_file("sha256", filepath));
+#elif CRYPTO_WITH_RIPEMD
+	return ("rmd160:" + FileHelper::libcrypto_hash_file("ripemd160", filepath));
+#elif CRYPTO_WITH_SHA
+	return ("sha1:" + FileHelper::libcrypto_hash_file("sha1", filepath));
+#else
+#error libcrypto supports none of the supported algorithms
+#endif
+#else
+#error no library for hashing found
+#endif
+}
+
+
+#if HAVE_CRYPTO
+std::string FileHelper::libcrypto_hash_file(const std::string& digest_name,
+											const std::string& filepath) {
+	OpenSSL_add_all_digests();
+	const EVP_MD *dgst_type = EVP_get_digestbyname(digest_name.c_str());
+
+	unsigned char hash_str[EVP_MAX_MD_SIZE];
+	unsigned int hash_len = 0;
+	std::stringstream oss;
+
+	EVP_MD_CTX *ctx = EVP_MD_CTX_create();
+
+	// Open file
+	FILE *file = fopen(filepath.c_str(), "r");
+	if (!file) {
+		perror(("fopen(" + filepath + ")").c_str());
+		goto bailout;
+	}
+
+	EVP_DigestInit_ex(ctx, dgst_type, NULL);
+
+	// Read file into digest update
+	{
+		void *buf = malloc(BUFSIZ);
+		if (!buf) {
+			perror(("malloc(" STRINGIZE(BUFSIZ) ")"));
+			goto bailout;
+		}
+
+		size_t bytes_read = 0;
+		while ((bytes_read = fread(buf, 1, BUFSIZ, file))) {
+			EVP_DigestUpdate(ctx, buf, bytes_read);
+		}
+
+		free(buf);
+	}
+
+	// Extract hash
+	EVP_DigestFinal_ex(ctx, hash_str, &hash_len);
+	EVP_MD_CTX_cleanup(ctx);
+
+	for (unsigned int i = 0; i < hash_len; ++i) {
+		oss << std::hex << (int)hash_str[i];
+	}
+
+	return oss.str();
+
+  bailout:
+	if (ctx) {
+		EVP_MD_CTX_cleanup(ctx);
+	}
+	return "";
+}
+#endif
--- src/filehelper.hxx
+++ src/filehelper.hxx
@@ -1,18 +1,36 @@
 #ifndef FILEHELPER_H
 #define FILEHELPER_H
 
+#ifdef HAVE_CONFIG_H
+#include "../config.h"
+#endif
+
+#include <sstream>
 #include <string>
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+
+#if HAVE_CRYPTO
+#include <openssl/evp.h>
+#endif
 
 class FileHelper {
 	public:
 	static std::string getWorkingDir();
 	static bool isValidRepoPath(/* cwd */);
 	static bool isValidRepoPath(const std::string& path);
+
+	static std::string hash_file(const std::string& filepath);
+
+	private:
+#if HAVE_CRYPTO
+	static std::string libcrypto_hash_file(const std::string& digest_name,
+										   const std::string& filepath);
+#endif
 };
 
 #endif
