17b563dccd7da00702a92d71ae6449a08800bb8c
Takashi Yoshi <takashi@yoshi.email>
2017-05-09 23:21:00 +0200

    Global cURL initialization

--- src/main.cxx
+++ src/main.cxx
@@ -1,9 +1,17 @@
+#if HAVE_LIBCURL
+#include <curl/curl.h>
+#endif
+
 #include "clicontroller.hxx"
 
 const char *DOLLAR_ZERO;
 
 int main(int argc, char *argv[]) {
 	DOLLAR_ZERO = argv[0];
+
+#if HAVE_LIBCURL
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+#endif
 
 	return CLIController::dispatch(argc, argv);
 }
