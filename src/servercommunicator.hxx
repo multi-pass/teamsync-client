1409e9d23c84bf60e5af8718cd0ca85b8dc68803
Takashi Yoshi <takashi@yoshi.email>
2017-05-11 22:23:22 +0200

    Add basic API communication

--- src/servercommunicator.hxx
+++ src/servercommunicator.hxx
@@ -1,13 +1,56 @@
 #ifndef SERVERCOMMUNICATOR_H
 #define SERVERCOMMUNICATOR_H
 
+#include <string>
+
+#include <stdio.h>
+#include <curl/curl.h>
+
 class ServerCommunicator {
 	public:
+	ServerCommunicator(std::string server_url);
 	bool authorize();
 	void getFullTree();
 	int addSecret();
 	int setSecret();
 	int deleteSecret();
+
+	private:
+	std::string server_url;
+};
+
+enum HTTPMethod {
+	GET,
+	POST,
+	PUT,
+	DELETE
+};
+
+class APIResponse;
+
+class APIRequest {
+	public:
+	APIRequest(HTTPMethod method, std::string server_url,
+			   std::string api_route);
+	APIResponse execute();
+
+	private:
+	CURL *curl;
+};
+
+class APIResponse {
+	friend class APIRequest;
+
+	public:
+	std::string http_response;
+
+	private:
+	APIResponse();
+	size_t receiveData(char *ptr, size_t size, size_t nmemb);
+
+	// only for passing to cURL
+	static size_t writeCallback(char *ptr, size_t size, size_t nmemb,
+								void *userdata);
 };
 
 #endif
