ce5fe3b03d8a8f8e02b62c9466ae5fcd9b09f0a1
Takashi Yoshi <takashi@yoshi.email>
2017-05-13 21:06:06 +0200

    Add Base64 library

---
+++ src/base64.hxx
@@ -0,0 +1,11 @@
+// http://www.cplusplus.com/forum/beginner/51572/#msg280295
+#ifndef BASE64_H
+#define BASE64_H
+
+#include <string>
+
+std::string base64_encode(unsigned char const *bytes_to_encode,
+						  unsigned int in_len);
+std::string base64_decode(std::string const& base64_string);
+
+#endif
