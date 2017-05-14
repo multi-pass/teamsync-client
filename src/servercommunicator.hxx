aefed497593de396bb06c80a041f4f4730e7d85f
Takashi Yoshi <takashi@yoshi.email>
2017-05-14 09:33:05 +0200

    Fixup ServerCommunicator::getFullTree method signature

--- src/servercommunicator.hxx
+++ src/servercommunicator.hxx
@@ -10,7 +10,8 @@ class ServerCommunicator {
 	public:
 	ServerCommunicator(const std::string& server_url);
 	bool authenticate(const std::string& pgpid);
-	void getFullTree();
+	void getFullTree(void (*callback)(const std::string&,
+									  const std::string&));
 	int addSecret();
 	int setSecret();
 	int deleteSecret();
