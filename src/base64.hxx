79adc7873fd737a4ab74a144ca25e75faa742c6d
Takashi Yoshi <takashi@yoshi.email>
2017-05-15 20:24:23 +0200

    Use size_t for string length in base64

--- src/base64.hxx
+++ src/base64.hxx
@@ -5,7 +5,7 @@
 #include <string>
 
 std::string base64_encode(unsigned char const *bytes_to_encode,
-						  unsigned int in_len);
+						  size_t in_len);
 std::string base64_decode(std::string const& base64_string);
 
 #endif
